ec3a0e62b993d0fadf86a0970b31493d518e7ef2
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2021-01-19 17:26:08 +0300

    mobile navigation setup

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,18 @@
 const buttonToggle = document.getElementById("toggle");
-const timesIcon = document.getElementsByClassName('fa-times');
-const barsIcon = document.getElementsByClassName('fa-bars');
-const barsIcon = document.getElementsByClassName('fa-bars');
-const menu = document.getElementsByClassName('menu');
+const crossIcon = document.getElementById('fa-times');
+const barsIcon = document.getElementById('fa-bars');
+const menu = document.getElementById('menu');
+
+barsIcon.addEventListener('click', function(){
+ menu.style.transform ="translateX(0)";
+ barsIcon.style.display = "none";
+ crossIcon.style.display="block";
+});
+
+crossIcon.addEventListener('click', function(){
+    menu.style.transform ="translateX(-100%)";
+    barsIcon.style.display = "block";
+    crossIcon.style.display="none";
+})
 
 
