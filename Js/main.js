4174538f48a55648e6269f2fb5470046fcbe9772
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2021-01-19 15:52:18 +0300

    creating mobile nav

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+const buttonToggle = document.getElementById("toggle");
+const timesIcon = document.getElementsByClassName('fa-times');
+const barsIcon = document.getElementsByClassName('fa-bars');
+const barsIcon = document.getElementsByClassName('fa-bars');
+const menu = document.getElementsByClassName('menu');
+
+
